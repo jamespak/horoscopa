da75fe2c348d375595e337ec3121eea534d7fe78
James Pak <mrjamespak@gmail.com>
2022-05-12 16:05:36 -0700

    initial project

---
+++ src/main/java/com/example/horoscopa/HoroscopaApplication.java
@@ -0,0 +1,13 @@
+package com.example.horoscopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoroscopaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoroscopaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/horoscopa/HoroscopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.horoscopa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoroscopaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
