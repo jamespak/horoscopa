19015a881f536f33adf12a481bea2dac48723c2a
James Pak <mrjamespak@gmail.com>
2022-05-13 10:06:42 -0700

    auto generating of postgres tables

--- src/main/java/com/example/horoscopa/entities/HoroscopeEntity.java
+++ src/main/java/com/example/horoscopa/entities/HoroscopeEntity.java
@@ -3,9 +3,11 @@ package com.example.horoscopa.entities;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.Date;
 
 @Entity
+@Table(name="horoscopes")
 public class HoroscopeEntity {
     @Id
     @Column(name = "id", nullable = false)
