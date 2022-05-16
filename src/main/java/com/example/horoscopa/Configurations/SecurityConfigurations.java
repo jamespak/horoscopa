da79425cfbdc6b503f42c6efa82aabd2dcfc8828
James Pak <mrjamespak@gmail.com>
2022-05-15 17:48:22 -0700

    adding WebSecurityConfigurerAdapter

--- src/main/java/com/example/horoscopa/Configurations/SecurityConfigurations.java
+++ src/main/java/com/example/horoscopa/Configurations/SecurityConfigurations.java
@@ -3,6 +3,10 @@ package com.example.horoscopa.Configurations;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,14 +22,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-@Configuration
-public class SecurityConfigurations {
+@EnableWebSecurity
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder encoder =
             PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
     @Bean
-    UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService() {
         UserDetails userDetails = User.builder()
                 .username(GetUserProperties().getProperty("user.name"))
                 .password(encoder.encode(GetUserProperties().getProperty("user.password")))
@@ -35,6 +39,18 @@ public class SecurityConfigurations {
             return new InMemoryUserDetailsManager(userDetails);
 
         }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(auth);
+        http.authorizeRequests()
+                .mvcMatchers(("/horoscope/**")).hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
 
     private Properties GetUserProperties() {
 
@@ -54,5 +70,7 @@ public class SecurityConfigurations {
         }
         return prop;
     }
+
+
 
 }
